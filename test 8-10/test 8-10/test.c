c8850bad5553ca20628446ee514f3918214a53cc
sabrina-LYY <sbrina930633839@163.com>
2019-08-16 17:00:27 +0800

    提交代码

---
+++ test 8-10/test 8-10/test.c
@@ -0,0 +1,22 @@
+
+
+//printf--输出函数
+//#include<stdio.h>
+//#include<stdlib.h>
+////main--主函数，程序的入口
+//int main()
+//{
+//	printf("hello world\n");
+//	//getchar();
+//	//system("pause");
+//	return 0;
+//}
+
+
+#include<stdio.h>
+
+int main()
+{
+	printf("%d\n", strlen("c:\test\328\test.c"));
+	return 0;
+}
